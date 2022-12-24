c9bf1982a70e161009753e355a905edb1e28ecd8
Perkles <rafael.arantes7@hotmail.com>
2022-12-23 21:38:34 -0300

    Projeto DIO

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import br.com.dio.desafio.Curso;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        System.out.println(curso1);
+    }
+
+}
